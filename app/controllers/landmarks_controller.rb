dca5036437a9fa39d189f05c4ab7af31a823aa5b
github username <github email address>
2018-11-01 19:21:10 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,17 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+
+  get '/landmarks/new' do
+    erb :"/landmarks/new"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params)
+    redirect to "/landmarks/#{@landmark.id}"  #redirect to
+  end
+
 end
